66ecffe6c231ec6c964d81e50be770a87fb51035
cephas-n <cephas.developer@gmail.com>
2023-04-05 16:20:31 +0300

    fetch platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,16 +1,29 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spinner,
+} from "@chakra-ui/react";
 import React from "react";
 import { FaChevronDown } from "react-icons/fa";
+import useData from "../hooks/useData";
+import usePlatform from "../hooks/usePlatform";
 
 const PlatformSelector = () => {
+  const { data: platforms, error, isLoading } = usePlatform();
+
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<FaChevronDown />} minWidth={20}>Platform</MenuButton>
+      <MenuButton as={Button} rightIcon={<FaChevronDown />} minWidth={20}>
+        Platform
+      </MenuButton>
       <MenuList>
-        <MenuItem>Item 1</MenuItem>
-        <MenuItem>Item 2</MenuItem>
-        <MenuItem>Item 3</MenuItem>
-        <MenuItem>Item 4</MenuItem>
+        {isLoading && <Spinner />}
+        {platforms.map((platform) => (
+          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
