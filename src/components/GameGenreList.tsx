55a9c56f793b6cd84ecb1180a4c764aff9af925d
cephas-n <cephas.developer@gmail.com>
2023-04-08 19:43:48 +0300

    cleanip genres

--- src/components/GameGenreList.tsx
+++ src/components/GameGenreList.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Heading,
   HStack,
   Image,
   List,
@@ -21,25 +22,33 @@ const GameGenreList = ({ onSelect, selectedGenre }: Props) => {
   if (isLoading) return <Spinner size="lg" />;
 
   return (
-    <List>
-      {genres.map((genre) => (
-        <HStack key={genre.id} paddingY="5px">
-          <Image
-            src={getCroppedImage(genre.image_background)}
-            boxSize="32px"
-            borderRadius="8px"
-          />
-          <Button
-            fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-            fontSize="lg"
-            variant="link"
-            onClick={() => onSelect(genre)}
-          >
-            {genre.name}
-          </Button>
-        </HStack>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {genres.map((genre) => (
+          <HStack key={genre.id} paddingY="5px">
+            <Image
+              src={getCroppedImage(genre.image_background)}
+              boxSize="32px"
+              borderRadius="8px"
+              objectFit="cover"
+            />
+            <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              fontSize="lg"
+              variant="link"
+              onClick={() => onSelect(genre)}
+              whiteSpace="normal"
+              textAlign="left"
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        ))}
+      </List>
+    </>
   );
 };
 
