a775c714d534b58f4b50651844ebf98110cf3875
cephas-n <cephas.developer@gmail.com>
2023-04-08 20:13:18 +0300

    Refactor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import _ from "lodash";
 import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
@@ -16,6 +16,8 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+
+  if (error) return <Text marginTop={5}>{error}</Text>;
 
   return (
     <SimpleGrid
